177a6f5cc19e4a47886f3ba61b58271981cfd238
ASHISH KUMAR <145495068+IamAshishSingh02@users.noreply.github.com>
2024-07-26 09:47:30 +0530

    constructor.cpp

--- types of constructor.cpp
+++ types of constructor.cpp
@@ -1,61 +1,68 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Student{
+class Student
+{
 private:
 string* name;
 int* age;
+
 public:
+    Student() //default constructor
+    {      
+        cout<< "Default Constructor called"<<endl;
+        name = new string;
+        *name = "Student";
+        age = new int;
+        *age = 00;
+    }
 
-Student(){      //default constructor
-   cout<< "Default Constructor called"<<endl;
-   name = new string;
-   *name = "Student";
-   age = new int;
-   *age = 00;
-}
- Student(string sname, int sage){     // parameterized constructor
-    cout<<"Paramertised constructor called"<< endl;
-    name = new string;
-    age = new int;
-    *name = sname;
-    *age = sage;
- };
+    Student(string sname, int sage) //parameterized constructor
+    {     
+        cout<<"Paramertised constructor called"<< endl;
+        name = new string;
+        age = new int;
+        *name = sname;
+        *age = sage;
+    };
 
- Student(Student& t){       // copy constructor
-    cout<<"Copy constructor called"<< endl;
-    age = new int;
-    name = new string;
-    *name = *t.name;
-    *age = *t.age;
- };
+    Student(Student& t) //copy constructor
+    {       
+        cout<<"Copy constructor called"<< endl;
+        age = new int;
+        name = new string;
+        *name = *t.name;
+        *age = *t.age;
+    };
 
- Student(Student&& other) noexcept{   //move constructor
-   cout<<"Move constructor called"<< endl;
-   age = other.age;
-   name = other.name;
-   other.name = nullptr;
-   other.age = nullptr;
-   }
+    Student(Student&& other) noexcept //move constructor
+    {   
+        cout<<"Move constructor called"<< endl;
+        age = other.age;
+        name = other.name;
+        other.name = nullptr;
+        other.age = nullptr;
+    }
 
-   ~Student(){
-      delete name;
-      delete age;
-   }
+    ~Student()
+    {
+        delete name;
+        delete age;
+    }
 
-
- void display(){
-    cout<<"This is the object of "<< *name << " whose age is "<< *age<<endl;
- }
+    void display()
+    {
+        cout<<"This is the object of "<<*name<<" whose age is "<<*age<<endl;
+    }
 };
 
-int main(){
+int main()
+{
     Student s("Ashish",20), s2("Anish",19), s3;
     Student Anish(s2);
-    s.display(); // calling parameterized constructor
-    Anish.display(); // calling copy constructor
-    s3.display(); // calling default constructor
+    s.display(); //calling parameterized constructor
+    Anish.display(); //calling copy constructor
+    s3.display(); //calling default constructor
     Student newS(move(s));
     newS.display();
-
 }
